563d31faf3f0811c7ed18003b1f328b5faa5211b
Demo User <demouser@MacBook-Pro.local>
2015-07-28 09:23:37 -0700

    getting alert to work

--- projectmemory/static/create.js
+++ projectmemory/static/create.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
   $("#submit").click(function(){
+  
       var x = window.confirm("Are you satisfied with this message? You cannot edit or view this message after you click submit.");
         if(x){
           /* the OK button should be a LINK (a href) to go to the (next) saved memories page */
-          window.location = "http://www.google.com"
+          window.location = "/profile"
         }
         else{
           /* close alert */
         }
+
   });
 
 });
