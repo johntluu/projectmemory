44b1867f563da9a93c0daba403fd728df01243e4
Catherine Kim <kimcatherine2297@gmail.com>
2015-07-29 15:04:46 -0700

    merged py and css

--- projectmemory/static/profile.js
+++ projectmemory/static/profile.js
@@ -1,16 +1,6 @@
 $(document).ready(function(){
-  $("#delete").click(function(){
-
-      var x = window.confirm("Are you sure you want to delete this message?");
-        if(x){
-          /* 1. delete this message
-             2. redirect to self, probably self.redirect('/profile') or
-                window.location = "/profile"*/
-        }
-        else{
-          /* close alert */
-        }
+  $('input[type="button"]').click(function(e){
+   $(this).closest('tr').remove()
+})
 
   });
-
-});
